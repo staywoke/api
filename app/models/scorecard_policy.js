030546231d75213659f3f112a844cdc481ab5858
tracethompson <tracethmpsn@gmail.com>
2020-02-27 14:16:33 -0500

    adds columns

--- app/models/scorecard_policy.js
+++ app/models/scorecard_policy.js
@@ -18,6 +18,96 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER(10).UNSIGNED,
       allowNull: false,
       comment: 'Foreign Key to scorecard_agency'
+    },
+    disqualifies_complaints: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_disqualifies_complaints: {
+      type: DataTypes.BOOLEAN
+    },
+    restricts_delays_interrogations: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_restricts_delays_interrogations: {
+      type: DataTypes.BOOLEAN
+    },
+    gives_officers_unfair_access_to_information: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_gives_officers_unfair_access_to_information: {
+      type: DataTypes.BOOLEAN
+    },
+    limits_oversight_discipline: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_limits_oversight_discipline: {
+      type: DataTypes.BOOLEAN
+    },
+    requires_city_pay_for_misconduct: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_requires_city_pay_for_misconduct: {
+      type: DataTypes.BOOLEAN
+    },
+    erases_misconduct_records: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_erases_misconduct_records: {
+      type: DataTypes.BOOLEAN
+    },
+    requires_deescalation: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_requires_deescalation: {
+      type: DataTypes.BOOLEAN
+    },
+    bans_chokeholds_and_strangleholds: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_bans_chokeholds_and_strangleholds: {
+      type: DataTypes.BOOLEAN
+    },
+    duty_to_intervene: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_duty_to_intervene: {
+      type: DataTypes.BOOLEAN
+    },
+    requires_warning_before_shooting: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_requires_warning_before_shooting: {
+      type: DataTypes.BOOLEAN
+    },
+    restricts_shooting_at_moving_vehicles: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_restricts_shooting_at_moving_vehicles: {
+      type: DataTypes.BOOLEAN
+    },
+    requires_comprehensive_reporting: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_requires_comprehensive_reporting: {
+      type: DataTypes.BOOLEAN
+    },
+    requires_exhaust_all_other_means_before_shooting: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_requires_exhaust_all_other_means_before_shooting: {
+      type: DataTypes.BOOLEAN
+    },
+    has_use_of_force_continuum: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_language_has_use_of_force_continuum: {
+      type: DataTypes.BOOLEAN
+    },
+    policy_manual_link: {
+      type: DataTypes.STRING
+    },
+    police_union_contract_link: {
+      type: DataTypes.STRING
     }
   }, {
     indexes: [
